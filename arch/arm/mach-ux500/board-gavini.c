99d61cffddcbf531133ac0498dce8ff914a00966
onioneq4 <mailcloud@wp.pl>
2022-06-13 18:52:33 +0200

    idk

--- arch/arm/mach-ux500/board-gavini.c
+++ arch/arm/mach-ux500/board-gavini.c
@@ -305,7 +305,7 @@ struct regulator *gp2a_vio_reg;
 static int __init gp2a_setup( struct device * dev);
 static void gp2a_pwr(bool on);
 
-static struct gp2a_platform_data gp2a_plat_data __initdata = {
+static struct gp2a_platform_data gp2a_plat_data = {
 	.ps_vout_gpio = PS_VOUT_GAVINI_R0_0,
 	.hw_setup = gp2a_setup,
 	.hw_pwr = gp2a_pwr,
@@ -464,7 +464,7 @@ static struct platform_device gavini_backlight_device_r0_3 = {
 
 static int __init immvibe_setup( void );
 
-static struct isa1200_platform_data isa1200_plat_data __refdata = {
+static struct isa1200_platform_data isa1200_plat_data = {
 	.mot_hen_gpio = MOT_HEN_GAVINI_R0_0,
 	.mot_len_gpio = MOT_LEN_GAVINI_R0_0,
 	.mot_clk = NULL,
